23a2604b776a1309c5b841f80f2ebe87a58f052a
Alex Barbosa <alex.andrew.barbosa@gmail.com>
2022-02-25 16:38:06 -0500

    inital dots commit

--- starter/level-one.js
+++ starter/level-one.js
@@ -2,3 +2,14 @@
  * DOTS: Level One
  *
  */
+let ball = document.querySelector(`.js-ball`);
+let scoreDisplay = document.querySelector(`.js-score`);
+let levelWinner = document.querySelector(`.level-winner`);
+let score = 0
+ball.addEventListener(`click`, () => {
+    score = score+10;
+    scoreDisplay.innerText = score
+        if (score >= 100) {
+            levelWinner.style.opacity = `1`
+        }
+})
--- starter/level-three.js
+++ starter/level-three.js
@@ -1,4 +1,20 @@
-/*
- * DOTS: Level Three
- *
- */
+let score = 0;
+let ball = document.querySelectorAll(".js-ball");
+let scoreDisplay = document.querySelector(".js-score");
+let levelWinner = document.querySelector(".level-winner");
+for (let i = 0; i <  ball.length; i++) {
+    ball[i].addEventListener("click", function () {
+        let tBallScore = ball[i].dataset.increment;
+        console.log(tBallScore);
+        bClicked(parseInt(tBallScore));
+    })   
+}
+let bClicked = (bSize) => {
+    score += bSize;
+    scoreDisplay.innerText = score;
+    if (score >= 100) {
+        levelWinner.style.opacity = "1";
+    }
+    let randomColor = Math.floor(Math.random()*16777215).toString(16);
+    document.querySelector('body').style.backgroundColor = "#" + randomColor;
+}
